897028d27864871d05303dd7ff3b870848b7b146
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-07-25 22:14:11 +0400

    added gif

--- app/src/main/java/com/leusapp/material_calendar/MainActivity.kt
+++ app/src/main/java/com/leusapp/material_calendar/MainActivity.kt
@@ -6,23 +6,23 @@ import com.leusapp.material_calendarview.CalendarView
 
 class MainActivity : FragmentActivity() {
 
-    lateinit var calendar: CalendarView
+    //lateinit var calendar: CalendarView
     val TAG = "myLogs"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        calendar = findViewById(R.id.calendar)
-        initAdapter()
+       // calendar = findViewById(R.id.calendar)
+       // initAdapter()
     }
 
     
     private fun initAdapter(){
-        calendar.setCustomAdapter(
-            CustomAdapter(this, calendar.getArrayCalendar(), R.drawable.material_style_btn, R.drawable.material_item_pressed)
-        )
-        calendar.notifyCustomAdapter()
+       // calendar.setCustomAdapter(
+       //     CustomAdapter(this, calendar.getArrayCalendar(), R.drawable.material_style_btn, R.drawable.material_item_pressed)
+       // )
+       // calendar.notifyCustomAdapter()
     }
 
 
